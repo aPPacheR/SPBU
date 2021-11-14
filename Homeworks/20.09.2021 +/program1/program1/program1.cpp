cac43a944e004cf1c9d903aba9050e906ab16221
aPPacheR <87997793+aPPacheR@users.noreply.github.com>
2021-11-14 14:38:34 +0300

    Add files via upload

---
+++ Homeworks/20.09.2021 +/program1/program1/program1.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+int main(int argc, char** argv) {
+
+	unsigned long long S = 0;
+	int min = 9999999999, n = 0;
+	for (unsigned long long i = 0; i < 1000000; i++) {
+		S = i * i;
+		if (S % 1000000 == 269696)
+			if (S < min) {
+				min = S;
+				n = i;
+			}		
+	}
+	std::cout << " Min number: " << n << ", min square : " << min << std::endl;
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
---
+++ Homeworks/20.09.2021 +/program2/program2/program2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string.h>
+
+bool palindrom(char s[]) {
+    int n = strlen(s);
+    for (int i = 0; i < n / 2; i++)
+        if (s[i] != s[n - 1 - i])
+            return false;
+    return true;
+}
+
+int main()
+{
+    char s[100];
+    while ("True") {
+        std::cout << "Wtite string: "; std::cin.getline(s, 100);
+
+        if (palindrom(s))
+            std::cout << s << " it is polindrom" << std::endl;
+        else
+            std::cout << s << "it isn't polindrom" << std::endl;
+
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
