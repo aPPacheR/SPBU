02fcaf4473d4e2803d80df9dda875d0665458785
aPPacheR <87997793+aPPacheR@users.noreply.github.com>
2021-11-14 14:36:10 +0300

    Add files via upload

---
+++ Homeworks/18.10.2021 +/program1/program1/program1.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int n, k = 0;
+    std::cout << "Write n: "; std::cin >> n;
+
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+            k = k + 1;
+        }
+        else {
+            n = n * 3 + 1;
+            k = k + 1;
+        }
+    }
+   
+
+    std::cout << std::endl;
+
+    std::cout << "Number of steps - " << k << std::endl;
+    std::cout << "Final number - " << n << std::endl;
+
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
+
