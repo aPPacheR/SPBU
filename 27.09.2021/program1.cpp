10bb1a17b90717cbdf0d5b4ab0107245e408092c
aPPacheR <87997793+aPPacheR@users.noreply.github.com>
2021-12-19 15:49:19 +0300

    Add files via upload

---
+++ 27.09.2021/program1.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    for (int a = 32; a <= 255; ++a) {
+        std::cout << char(a) << " - " << a << std::endl;
+
+    }
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
---
+++ 27.09.2021/program2.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+
+int Akk(unsigned int m, unsigned int n) {
+    if (m == 0)
+        return (n + 1);
+
+    else if ((m > 0) && (n == 0))
+        return Akk(m - 1, 1);
+
+    else if ((m > 0) && (n > 0))
+        return Akk(m - 1, Akk(m, n - 1));
+
+    else
+        return 0;
+}
+
+int main()
+{
+    int m = 0, n = 0;
+
+    do {
+        std::cout << "Write positive number m: "; std::cin >> m;
+    } while (m < 0);
+
+    do {
+        std::cout << "Write positive numbers n: "; std::cin >> n;
+    } while (n < 0);
+
+    std::cout << std::endl;
+
+    std::cout << "Akk = " << Akk(m, n) << std::endl;
+
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
