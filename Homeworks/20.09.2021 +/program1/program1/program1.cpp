062f1404bf3c3805ff6f85cf369541f696528731
aPPacheR <87997793+aPPacheR@users.noreply.github.com>
2021-12-19 14:06:14 +0300

    Update program1.cpp

--- Homeworks/20.09.2021 +/program1/program1/program1.cpp
+++ Homeworks/20.09.2021 +/program1/program1/program1.cpp
@@ -1,9 +1,10 @@
-﻿#include<iostream>
+#include <iostream>
 
 int main(int argc, char** argv) {
 
 	unsigned long long S = 0;
-	int min = 9999999999, n = 0;
+	unsigned long long int min = -1;
+	int n = 0;
 	for (unsigned long long i = 0; i < 1000000; i++) {
 		S = i * i;
 		if (S % 1000000 == 269696)
