61e401916b906177ab49469a9a6d791dcb2632f9
aPPacheR <87997793+aPPacheR@users.noreply.github.com>
2021-12-20 11:17:07 +0300

    Update program1.cpp

--- 08.11.2021/program1.cpp
+++ 08.11.2021/program1.cpp
@@ -9,16 +9,16 @@ int main()
 	size_t x0 = 0, y0 = 0, x1 = col - 1, y1 = row - 1, x = 0, y = 0;
 	unsigned int count = 1;
 	while (y0 <= y1) {
-		while (x < x1) M[y][x++] = count++;
+		while (x < x1 && count <= row*col) M[y][x++] = count++;
 		y0++;
-		while (y < y1) M[y++][x] = count++;
+		while (y < y1 && count <= row*col) M[y++][x] = count++;
 		x1--;
-		while (x > x0) M[y][x--] = count++;
+		while (x > x0 && count <= row*col) M[y][x--] = count++;
 		y1--;
-		while (y > y0) M[y--][x] = count++;
+		while (y > y0 && count <= row*col) M[y--][x] = count++;
 		x0++;
 	}
-	M[y][x] = count;
+	if(count <= row*col) M[y][x] = count;
 	for (size_t i = 0; i < row; ++i) {
 		for (size_t j = 0; j < col; ++j) {
 			std::cout << M[i][j] << ' ';
