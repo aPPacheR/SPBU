458642891d47b9f1e1e8ee22995e043fe5e75717
aPPacheR <87997793+aPPacheR@users.noreply.github.com>
2021-12-27 02:11:15 +0300

    Update program3.cpp

--- 18.10.2021/program3.cpp
+++ 18.10.2021/program3.cpp
@@ -10,8 +10,8 @@ int main()
         std::cout << std::endl;
     } while ((n > 4000) || (n < 0));
 
-        // M = 1000
-	int k = n / 1000;
+    // M = 1000
+    int k = n / 1000;
     if (k != 0) {
 
         for (int i = 0; i < k; i++) {
@@ -22,24 +22,25 @@ int main()
     // C = 100
     k = n / 100;
     if (k != 0) {
-
-        if (k == 5) {
-            s = s + "D";
+        switch (k % 10) {
+        case 1: s = s + "C"; break;
+        case 2: s = s + "CC"; break;
+        case 3: s = s + "CCC"; break;
+        case 4: s = s + "CD"; break;
+        case 5: s = s + "D"; break;
+        case 6: s = s + "DC"; break;
+        case 7: s = s + "DCC"; break;
+        case 8: s = s + "DCCC"; break;
+        case 9: s = s + "CM"; break;
         }
-        else {
-            for (int i = 0; i < k; i++) {
-                s = s + "C";
-            }
-        }
-        n = n % 100;
     }
     // X = 10
     k = n / 10;
     if (k != 0) {
-        switch (k % 10){
+        switch (k % 10) {
         case 1: s = s + "X"; break;
-        case 2: s = s + "XI"; break;
-        case 3: s = s + "XII"; break;
+        case 2: s = s + "XX"; break;
+        case 3: s = s + "XXX"; break;
         case 4: s = s + "XL"; break;
         case 5: s = s + "L"; break;
         case 6: s = s + "LX"; break;
@@ -50,23 +51,22 @@ int main()
     }
     k = n / 1;
     if (k != 0) {
-	    switch (k % 10){
-	    case 1: s = s + "I"; break;
-	    case 2: s = s + "II"; break;
-	    case 3: s = s + "III"; break;
-	    case 4: s = s + "IV"; break;
-	    case 5: s = s + "V"; break;
-	    case 6: s = s + "VI"; break;
-	    case 7: s = s + "VII"; break;
-	    case 8: s = s + "VIII"; break;
-	    case 9: s = s + "IX"; break;
-	    }
-
+        switch (k % 10) {
+        case 1: s = s + "I"; break;
+        case 2: s = s + "II"; break;
+        case 3: s = s + "III"; break;
+        case 4: s = s + "IV"; break;
+        case 5: s = s + "V"; break;
+        case 6: s = s + "VI"; break;
+        case 7: s = s + "VII"; break;
+        case 8: s = s + "VIII"; break;
+        case 9: s = s + "IX"; break;
         }
+
+    }
     std::cout << "Your number in the Roman numeral system: " << s << std::endl;
 
     std::cout << std::endl;
-   
+
     return 0;
 }
-
